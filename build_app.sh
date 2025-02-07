a0d4ca8c8c63140c73f4031a773ede342b1125da
Luca Ballore <luca.ballore@gmail.com>
2025-02-07 22:26:23 +0000

    build: fix build script and update node version

--- build_app.sh
+++ build_app.sh
@@ -9,15 +9,24 @@ ENTRY_POINT="app.js"
 # Define the output binary name
 OUTPUT_BINARY="./bin/lgtv"
 
-# Check if nexe is installed
+# Check if pkg is installed
 if ! command -v pkg &> /dev/null
 then
     echo "pkg could not be found, installing..."
     npm install -g pkg
 fi
 
+# Check if target parameter is provided
+if [ -z "$1" ]
+then
+    echo "No target provided. Usage: ./build_app.sh <target> (e.g. node18-linux-arm64)"
+    exit 1
+fi
+
+TARGET=$1
+
 # Build the application using pkg
 echo "Building the application..."
-pkg $ENTRY_POINT -o $OUTPUT_BINARY
+pkg $ENTRY_POINT -o $OUTPUT_BINARY -t $TARGET
 
 echo "Build completed successfully."
